4d5441faf165adaa316a30313aed3511a46f95cb
Pierre Bretéché <pbreteche@dawan.fr>
2025-04-14 12:08:12 +0200

    Première route avec contrôleur

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return new Response('<html lang="en"><body><h1>Hello !</h1></body></html>');
+    }
+}
