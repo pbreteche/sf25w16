165683c7484329678ba5a32a4a7eaa0fcf25c169
Pierre Bretéché <pbreteche@dawan.fr>
2025-04-15 12:10:21 +0200

    Utilisation basique d'importmap

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Repository\PostRepository;
-use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
