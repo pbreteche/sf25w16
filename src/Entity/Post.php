48cb3fe4d35d95caf3022fdc4324144f375d2311
Pierre Bretéché <pbreteche@dawan.fr>
2025-04-18 13:48:15 +0200

    Doctrine Lifecycle callback

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -11,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Post
 {
     #[ORM\Id]
@@ -137,5 +138,11 @@ class Post
         $this->author = $author;
 
         return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function prePersist(): void
+    {
+        $this->createdAt ??= new \DateTimeImmutable();
     }
 }
